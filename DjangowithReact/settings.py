f32dba244aeb33a0c672ae5ed26c6311984ae1ee
kumarlm <turnitsimple001@gmail.com>
2021-09-18 11:13:46 +0530

    django locals at end

--- DjangowithReact/settings.py
+++ DjangowithReact/settings.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 import os
 import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +127,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
